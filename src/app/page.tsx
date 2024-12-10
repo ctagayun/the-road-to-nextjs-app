d7d7451190db563e6b3a4974304dec3d148ab14b
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-12-09 21:01:07 -0600

    Initial save Link Component section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
-   //* arrow function withoutparameters
+import Link from "next/link";
+
    const HomePage = () => {
-     return <h2 className="text-6xl"> Home Page. </h2>
- } 
+      
+        return(
+             <div>
+              <h2 className="text-6xl"> Home Page. </h2>
+              <Link href="/tickets" className="underline">
+                Go to Tickets
+               </Link>
+           </div>
+          )
+    
+ };
 
   export default HomePage;
