e0255c831107f6e663b77c3eaf0010091e3fdeeb
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-12-10 04:38:54 -0600

    Save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
 
-   const HomePage = () => {
-      
-        return(
-             <div>
-              <h2 className="text-6xl"> Home Page. </h2>
-              <Link href="/tickets" className="underline">
-                Go to Tickets
-               </Link>
-           </div>
-          )
-    
- };
+const HomePage = () => {
+  return (
+    <div>
+      <h2 className="text-lg">HomePage</h2>
 
-  export default HomePage;
+      <Link href="/tickets" className="underline">
+        Go to Tickets
+      </Link>
+    </div>
+  );
+};
+
+export default HomePage;
